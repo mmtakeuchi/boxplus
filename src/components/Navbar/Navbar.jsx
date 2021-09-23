c21bd5bb68affedbc1ac58ebb0491cf10d806626
mmtakeuchi <mikemtaki@gmail.com>
2021-09-23 14:18:46 -0700

    add link to navbar to go to home page when logo is clicked

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <nav className="navbar">BoxPlus</nav>;
+  return (
+    <nav className="navbar">
+      <Link to="/" className="link">
+        BoxPlus
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
